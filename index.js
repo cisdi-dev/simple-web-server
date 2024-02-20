599d7235f6a1fec9ff068615c8c8051a33187f1c
dikletscode <dikyrain0@gmail.com>
2024-02-20 12:11:21 +0700

    fix url

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = 8080;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
